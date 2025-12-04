e2beae4291b01b974dfaba793a236e0f4cc86db0
AbdulRehmanZia <abdulrehmanzia2021@gmail.com>
2025-12-04 20:34:15 +0500

    Frontend CORS bug fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ import helmet from "helmet"
 const app = express()
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:8080', 'https://pos-portal-pi.vercel.app', 'https://ims-k9x9.onrender.com'], 
+  origin: ['http://localhost:5173', 'http://localhost:8080', 'https://pos-portal-pi.vercel.app', 'https://ims-cyan-psi.vercel.app'], 
   credentials: true, // for cookies 
 }));
 
